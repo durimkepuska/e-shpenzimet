db978322e5a28a9581ecf29956cd227f730356b2
durimkepuska <durimkepuska@gmail.com>
2016-03-08 14:50:03 +0100

    v 1.5

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,1 +1,8 @@
-Click here to reset your password:  {{ url('password/reset/'.$token) }}
+<p>Pershendetje,</p>
+<p>
+  Ky eshte nje email automatike, mos ktheni email ketu.
+</p>
+
+<p>Kliko ne linkun me poshte per ta ndryshuar fjalekalimin:<br><br>  {{ url('password/reset/'.$token) }}</p>
+
+<p>e-Shpenzimet</p>
