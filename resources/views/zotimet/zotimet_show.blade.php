c41ca9f559589567dfbf0bdc1d92085b009bf513
durimkepuska <durimkepuska@gmail.com>
2016-05-11 11:31:21 +0200

    update tjeter3

--- resources/views/zotimet/zotimet_show.blade.php
+++ resources/views/zotimet/zotimet_show.blade.php
@@ -13,11 +13,11 @@
 
       <div class="well well-lg"  style="line-height:25px;">
         <p>Drejtoria për: {!! $data->Department->department !!}</p>
-          <p>Mjetet jane financuar nga: {!! $data->payment_source->payment_source !!} </p>
+          <p>Mjetet jane financuar nga: {!! $data->payment_source !!} </p>
         <p><strong>Data e planifikuar: {!! $data->expenditure_date !!}</strong> </p>
 
         <p>Regjistruar me: {!! $data->created_at !!}</p>
-        <p>Lloji i shpenzimit: {!! $data->Spendingtype !!}</p>
+        <p>Lloji i shpenzimit: {!! $data->Spendingtype->spendingtype !!}</p>
 
 
 
