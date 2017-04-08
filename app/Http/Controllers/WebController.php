80013d706a2d32d68eb047ec6ea13ce337c5da08
durim <durimkepuska@gmail.com>
2017-04-08 02:06:31 +0200

    second try year 2017

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -34,7 +34,7 @@ class WebController extends Controller
       $department_id = Input::get('department');
 
       $data = expenditure::where('expenditures.department_id' , $department_id)
-        ->where('hidde', 0)->where(DB::raw('YEAR(expenditures.created_at) = '.date('Y')))
+        ->where('hidde', 0)->where(DB::raw('YEAR(expenditures.created_at) = 2017'))
         ->join('departments', 'expenditures.department_id', '=', 'departments.id')
         ->join('suppliers', 'expenditures.supplier_id', '=', 'suppliers.id')
         ->join('spendingtypes', 'expenditures.spendingtype_id', '=', 'spendingtypes.id')
