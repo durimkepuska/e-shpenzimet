42334d8bf06430aa854db5806a31ec7eb2a0a7df
durim <durim@imbus-peja.de>
2019-12-12 15:29:36 +0100

    added pie charts

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -154,10 +154,15 @@ xAxis: {
 },
 tooltip: {
   headerFormat: '<span style="font-size:11px"> <b>{point.y:,.2f} EUR</b></span><br>',
-  pointFormat: '<span >{point.name}</span>'
+  pointFormat: '{series.name}: <span >{point.name}</span>'
 },
 
 plotOptions: {
+ pie: {
+          
+           
+            showInLegend: true
+        }
   series: {
 
   borderWidth:1,
