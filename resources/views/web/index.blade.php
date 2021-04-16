c2669743189c111861e902c8724c903cda20ad1d
durimkepuska <durimkepuska@users.noreply.github.com>
2021-04-16 14:30:21 +0200

    Update index.blade.php

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -73,11 +73,13 @@ if(isset($_GET['chart'])){
 	        <li class="dropdown">
 	          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Vitet <span class="caret"></span></a>
 	          <ul class="dropdown-menu">
+		    <li ><a onclick="setGetParameter('year','2015')" >2015</a></li>
 	            <li ><a onclick="setGetParameter('year','2016')" >2016</a></li>
 	            <li ><a onclick="setGetParameter('year','2017')" >2017</a></li>
 	            <li ><a onclick="setGetParameter('year','2018')" >2018</a></li>
 	            <li ><a onclick="setGetParameter('year','2019')" >2019</a></li>
 	            <li ><a onclick="setGetParameter('year','2020')" >2020</a></li>
+		    <li ><a onclick="setGetParameter('year','2021')" >2021</a></li>
 			  </ul>
 	        </li>
 	        <li class="dropdown">
